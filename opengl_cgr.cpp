b54e66e46d0205f6d7a765caf02a653de26cd128
Magdalena019 <32893935+Magdalena019@users.noreply.github.com>
2018-01-20 10:24:30 +0100

    working

---
+++ opengl_cgr.cpp
@@ -0,0 +1,319 @@
+//
+//  opengl_cgr.cpp
+//
+//  Created by Magdalena Geißbauer on 09/01/2018.
+//  Copyright © 2018 ue. All rights reserved.
+//
+
+#include <GLUT/GLUT.h>
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wshorten-64-to-32"
+
+
+float move = 0.0f;
+float first = 0.0f;
+float second = 0.0f;
+float bucket = 0.0f;
+
+float preX = 0.0f;
+float rotation = 0.0f;
+
+// rotates everything
+void onMotion(int x, int y) {
+    if (fabs(x - preX))    {
+        rotation = ((x - preX) > 0) ? rotation += 10.0 * 0.11 : rotation -= 10.0 * 0.11;
+    }
+    
+    preX = x;
+    glutPostRedisplay();
+}
+
+// mouse input with left button pressed
+void onMouse(int button, int state, int x, int y) {
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+        preX = x;
+        glutMotionFunc(onMotion);
+    }
+}
+
+void description()
+{
+    std::cout << "You move the excavot in one direction. --> m" << std::endl;
+    std::cout << "You move up the first arm. --> f" << std::endl;
+    std::cout << "You move the first arm down --> F" << std::endl;
+    std::cout << "You move up the second arm. --> s" << std::endl;
+    std::cout << "You move the wirst second down --> S" << std::endl;
+    std::cout << "You open the bucket --> o" << std::endl;
+    std::cout << "You clorse the bucket --> c" << std::endl;
+}
+
+
+// EXCAVATOR - PARTS
+// -----------------------------------------
+
+// ----- drawing the arm combined with the spheres -----
+void createArmWithSphere()
+{
+    glPushMatrix();
+    glTranslatef(-1.5, 0, 0);
+    glutWireSphere(0.8, 10, 10);
+    glPopMatrix();
+    
+    glPushMatrix();
+    glScaled(3.0, 1, 1);
+    glutWireCube(1);
+    glPopMatrix();
+}
+
+// ----- drawing the bucket -----
+void createBucket()
+{
+    glPushMatrix();
+    glTranslatef(-0.6, -0.1, 0);
+    glScaled(1.1, 0.7, 1.3);
+    glutWireCube(2);
+    glPopMatrix();
+    
+    
+    glPushMatrix();
+    glTranslatef(-1.7, -0.4, 0);
+    glScaled(0.07, 1, 1.5);
+    glutWireCube(2);
+    glPopMatrix();
+}
+
+// ----- drawing the excavator arm -----
+void createExcavatorArmAndBucket () {
+    glPushMatrix();
+    glRotatef(-45, 0, 0, 1);
+    
+    createArmWithSphere();
+    glTranslatef(-1.5, 0, 0);
+    glRotatef(first, 0, 0, 1);
+    glTranslatef(1.5, 0, 0);
+    
+    glTranslatef(-1.5, 0, 0);
+    glRotatef(40, 0, 0, 1);
+    glTranslatef(-1.5, 0, 0);
+    
+    createArmWithSphere();
+    glTranslatef(-1.5, 0, 0);
+    glRotatef(second, 0, 0, 1);
+    glTranslatef(1.5, 0, 0);
+    
+    glTranslatef(-1.5, 0, 0);
+    glRotatef(80, 0, 0, 1);
+    glTranslatef(-1.5, 0, 0);
+    
+    createArmWithSphere();
+    glTranslatef(-1.5, 0, 0);
+    glRotatef(bucket, 0, 0, 1);
+    glTranslatef(1.5, 0, 0);
+    
+    glTranslatef(-1.3, 0, 0);
+    glRotatef(90, 0, 0, 1);
+    glTranslatef(-0.4, 0.6, 0);
+    createBucket();
+    
+    glPopMatrix();
+}
+
+
+// ----- drawing the cabin -----
+void createExcavatorCabin()
+{
+    glPushMatrix();
+    glTranslatef(5, 1, 2);
+    glScaled(1, 1.3, 1);
+    glutWireCube(2);
+    glPopMatrix();
+    
+    
+    glPushMatrix();
+    glTranslatef(0.1, -0.9, 2);
+    glScaled(0.8, 1, 1);
+    glutWireCube(1);
+    glPopMatrix();
+    
+    glPushMatrix();
+    glTranslatef(3, -0.8, 2);
+    glScaled(1.3, 0.5, 1);
+    glutWireCube(4);
+    glPopMatrix();
+    
+    
+    glTranslatef(1, -1.5, -2);
+    
+    
+}
+
+// ----- draw the wheel -----
+void createWheels()
+{
+    glPushMatrix();
+    glScaled(1.2, 1.2, 0.5);
+    glutWireSphere(1, 10, 10);
+    glPopMatrix();
+}
+
+
+// ----- drawing the excavator -----
+void drawExcavator()
+{
+    glTranslatef(2, 1, -16);
+    
+    createExcavatorArmAndBucket();
+    
+    glTranslatef(1, -0.35, 0);
+    createExcavatorCabin();
+    
+    glPushMatrix();
+    glTranslatef(2, -1, -3);
+    createWheels();
+    glTranslatef(4, 0, 0);
+    createWheels();
+    glPopMatrix();
+    
+    glPushMatrix();
+    glTranslatef(2, -1, -1);
+    createWheels();
+    glTranslatef(4, 0, 0);
+    createWheels();
+    glPopMatrix();
+}
+
+// draw surface
+void drawSurface()
+{
+    glPushMatrix();
+    
+    glTranslatef(0, -2, -15);
+    glScaled(25, 0.1, 10);
+    glutWireCube(1);
+    
+    glPopMatrix();
+}
+
+// renders everything
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
+    glColor3f(1.0, 1.0, 1.0);
+    
+    glRotatef(rotation, 0, 1, 0);
+    
+    drawSurface();
+    
+    glTranslatef(move, 0, 0);
+    drawExcavator();
+    
+    glFlush();
+}
+
+// what it does with what key
+void keysPressed(unsigned char key, int x, int y)
+{
+    switch (key) {
+            
+        case 'm':
+            move -= 0.3;
+            break;
+            
+        case 'h':
+            description();
+            break;
+            
+        case 'f':
+            if (first > -60) // collision avoidance
+            {
+                first -= 10;
+            }
+            break;
+            
+        case 'F':
+            if (first < 40) // collision avoidance
+            {
+                first += 10;
+            }
+            break;
+            
+        case 's':
+            if (second > -80) // collision avoidance
+            {
+                second -= 10;
+            }
+            break;
+            
+        case 'S':
+            if (second < 50) // collision avoidance
+            {
+                second += 10;
+            }
+            break;
+            
+        case 'o':
+            if (bucket < 20) // collision avoidance
+            {
+                bucket += 10;
+            }
+            break;
+            
+        case 'c':
+            if (bucket > -120) // collision avoidance
+            {
+                bucket -= 10;
+            }
+            break;
+    }
+    
+    glutPostRedisplay();
+}
+
+//initialization of the application. only started once.
+void init()
+{
+    //select clearing color (color that is used as 'background')
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    
+    //initialize view
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    
+    float width = glutGet(GLUT_WINDOW_WIDTH);
+    float height = glutGet(GLUT_WINDOW_HEIGHT);
+    gluPerspective(65, width/height, 0.1, 60);
+    
+    //set matrix-mode back to model-view for rendering
+    glMatrixMode(GL_MODELVIEW);
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    
+    //initialize display mode (single buffer and RGBA)
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    
+    //initialize window size and position. open window
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(200, 200);
+    glutCreateWindow("OpenGL - CGR");
+    
+    //call initialization routine
+    init();
+    
+    glutKeyboardFunc(keysPressed);
+    glutMouseFunc(onMouse);
+    glutDisplayFunc(display);
+    
+    //enter main loop and process events
+    glutMainLoop();
+    
+    return 0;
+}
+
--- s1610238019_Geissbauer_MTD215_06/opengl_cgr.cpp
+++ s1610238019_Geissbauer_MTD215_06/opengl_cgr.cpp
@@ -2,7 +2,16 @@
 #include <GLUT/GLUT.h>
 #include <time.h>
 #include <math.h>
+#include <iostream>
 // #include <windows.h>
+
+float move = 0.0f;
+float first = 0.0f;
+float second = 0.0f;
+float bucket = 0.0f;
+
+float preX = 0.0f;
+float rotation = 0.0f;
 
 //ROTATION_SPEED1 and ROTATION_SPEED2 can be used to change the rotation speed of the two small outer spheres
 #define ROTATION_SPEED1 -150.0
@@ -11,6 +20,9 @@
 //materialSpecular and materialShininess can be used to change the initial settings of the light source
 static const GLfloat materialSpecular[] = {1.0, 1.0, 1.0, 1.0};
 static const GLfloat materialShininess = 20.0;
+
+static const GLfloat materialAmbient[] = {0.0, 0.0, 0.0, 0.0};
+static const GLfloat materialDiffuse = 0.0;
 
 //the following colors can be used to set either the ambient or diffuse material color
 static const GLfloat red[] = {1.0, 0.0, 0.0, 1.0};
@@ -47,12 +59,217 @@ static int moving, startx, starty;
 static int windowWidth = 320;
 static int windowHeight = 240;
 
-//setup all initial settings for OpenGL
+// rotates everything
+void onMotion(int x, int y) {
+    if (fabs(x - preX))    {
+        rotation = ((x - preX) > 0) ? rotation += 10.0 * 0.11 : rotation -= 10.0 * 0.11;
+    }
+    
+    preX = x;
+    glutPostRedisplay();
+}
+
+// mouse input with left button pressed
+void onMouse(int button, int state, int x, int y) {
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+        preX = x;
+        glutMotionFunc(onMotion);
+    }
+}
+
+void description()
+{
+    std::cout << "You move the excavot in one direction. --> m" << std::endl;
+    std::cout << "You move up the first arm. --> f" << std::endl;
+    std::cout << "You move the first arm down --> F" << std::endl;
+    std::cout << "You move up the second arm. --> s" << std::endl;
+    std::cout << "You move the wirst second down --> S" << std::endl;
+    std::cout << "You open the bucket --> o" << std::endl;
+    std::cout << "You clorse the bucket --> c" << std::endl;
+}
+
+// EXCAVATOR - PARTS
+// -----------------------------------------
+
+// ----- drawing the arm combined with the spheres -----
+void createArmWithSphere()
+{
+    glPushMatrix();
+    glColor3f(0.9, 0.9, 0.0);
+    glScaled(3.0, 1, 1);
+    glutSolidCube(1);
+    //glutWireCube(1);
+    glPopMatrix();
+    
+    glPushMatrix();
+    glColor3f(0.0, 0.0, 0.0); // TODO: Change color
+    glTranslatef(-1.5, 0, 0);
+    glutSolidSphere(0.8, 10, 10);
+    //glutWireSphere(0.8, 10, 10);
+    glPopMatrix();
+    
+}
+
+void createBucket()
+{
+    glPushMatrix();
+    glTranslatef(-0.6, -0.1, 0);
+    glScaled(1.1, 0.7, 1.3);
+    glutSolidCube(2);
+    glPopMatrix();
+    
+    
+    glPushMatrix();
+    glTranslatef(-1.7, -0.4, 0);
+    glScaled(0.07, 1, 1.5);
+    glutSolidCube(2);
+    glPopMatrix();
+}
+
+void createExcavatorArmAndBucket () {
+    glPushMatrix();
+    glRotatef(-45, 0, 0, 1);
+    
+    createArmWithSphere();
+    glTranslatef(-1.5, 0, 0);
+    glRotatef(first, 0, 0, 1);
+    glTranslatef(1.5, 0, 0);
+    
+    glTranslatef(-1.5, 0, 0);
+    glRotatef(40, 0, 0, 1);
+    glTranslatef(-1.5, 0, 0);
+    
+    createArmWithSphere();
+    glTranslatef(-1.5, 0, 0);
+    glRotatef(second, 0, 0, 1);
+    glTranslatef(1.5, 0, 0);
+    
+    glTranslatef(-1.5, 0, 0);
+    glRotatef(80, 0, 0, 1);
+    glTranslatef(-1.5, 0, 0);
+    
+    createArmWithSphere();
+    glTranslatef(-1.5, 0, 0);
+    glRotatef(bucket, 0, 0, 1);
+    glTranslatef(1.5, 0, 0);
+    
+    glColor3f(0.9, 0.9, 0.0);
+    glTranslatef(-1.3, 0, 0);
+    glRotatef(90, 0, 0, 1);
+    glTranslatef(-0.4, 0.6, 0);
+    createBucket();
+    
+    glPopMatrix();
+}
+
+void createExcavatorCabin()
+{
+    glPushMatrix();
+    glTranslatef(5, 1, 2);
+    glScaled(1, 1.3, 1);
+    glutSolidCube(2);
+    glPopMatrix();
+    
+    
+    glPushMatrix();
+    glTranslatef(0.1, -0.9, 2);
+    glScaled(0.8, 1, 1);
+    glutSolidCube(1);
+    glPopMatrix();
+    
+    glPushMatrix();
+    glTranslatef(3, -0.8, 2);
+    glScaled(1.3, 0.5, 1);
+    glutSolidCube(4);
+    glPopMatrix();
+    
+    
+    glTranslatef(1, -1.5, -2);
+    
+    
+}
+
+void createWheels()
+{
+    glPushMatrix();
+    glScaled(1.2, 1.2, 0.5);
+    glutSolidSphere(1, 20, 20);
+    glPopMatrix();
+}
+
+
+void drawExcavator()
+{
+    glTranslatef(-3, 1, -16);
+    
+    createExcavatorArmAndBucket();
+
+    
+    glColor3f(0.0, 0.0, 0.0);
+    glPushMatrix();
+    glTranslatef(1, -2, -3);
+    createWheels();
+    glTranslatef(5.5, 0, 0);
+    createWheels();
+    glPopMatrix();
+    
+    glColor3f(0.9, 0.9, 0.0);
+    glTranslatef(1, -0.35, 0);
+    createExcavatorCabin();
+    
+    glColor3f(1.0, 0.0, 0.0);
+    glPushMatrix();
+    glTranslatef(0, -1, -1);
+    createWheels();
+    glTranslatef(5.5, 0, 0);
+    createWheels();
+    glPopMatrix();
+    
+}
+
+void drawSurface()
+{
+    glPushMatrix();
+    glColor3f(0.2, 0.2, 0.2);
+    glTranslatef(0, -2, -15);
+    glScaled(25, 0.1, 10);
+    glutSolidCube(1);
+    
+    glPopMatrix();
+}
+
+
 static void init(void) 
 {
+    //select clearing color (color that is used as 'background')
 	glClearColor (0.0, 0.0, 0.0, 0.0);
+    
+    
 	glShadeModel (GL_SMOOTH);
 	glEnable(GL_DEPTH_TEST);
+    glEnable(GL_LIGHTING);
+    
+    glEnable(GL_LIGHT0);
+    //glEnable(GL_LIGHT1);
+    
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, materialSpecular);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, materialShininess);
+    
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, red);
+    glMaterialf(GL_FRONT_AND_BACK, GL_DIFFUSE, green[3]);
+    
+     
+    //initialize view
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    
+    float width = glutGet(GLUT_WINDOW_WIDTH);
+    float height = glutGet(GLUT_WINDOW_HEIGHT);
+    gluPerspective(65, width/height, 0.1, 60);
+    
+    //set matrix-mode back to model-view for rendering
+    glMatrixMode(GL_MODELVIEW);
+    
 }
 
 //renders a sphere with the given radius at the specified position
@@ -74,16 +291,30 @@ void setMaterial(const GLfloat *materialDiffuse, const GLfloat *materialAmbient)
 //renders the whole scene conatining three spheres (two small and one larger sphere)
 static void display(void) 
 {
-	glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    // | GL_DEPTH_BUFFER_BIT
+	glClear (GL_COLOR_BUFFER_BIT );
 	glLoadIdentity();
+    
+    glColor3f(1.0, 1.0, 1.0);
+    
+    glRotatef(rotation, 0, 1, 0);
 
-	glTranslated(scenePosition[0], scenePosition[1], scenePosition[2]);
+    drawSurface();
+    
+	// glTranslated(scenePosition[0], scenePosition[1], scenePosition[2]);
 
+    /*
 	renderSphere(1.0, 0, 0, 0.0);
 	renderSphere(0.5, -3, 0.0, 0.0);
 	renderSphere(0.5, 0, 2, 0.0);
+     */
+    
+    glTranslatef(move, 0, 0);
+    drawExcavator();
+    
+    glFlush();
 
-	glutSwapBuffers();
+	//glutSwapBuffers();
 }
 
 static void reshape (int w, int h) 
@@ -103,20 +334,114 @@ static void keyboard(unsigned char key, int x, int y)
 		case 27:
 			exit(0);
 			break;
+            
+        case 'm':
+            move -= 0.3;
+            break;
+        
+        case 'M':
+            move += 0.3;
+            break;
+            
+        case 'h':
+            description();
+            break;
+            
+        case 'f':
+            if (first > -60) // collision avoidance
+            {
+                first -= 10;
+            }
+            break;
+            
+        case 'F':
+            if (first < 40) // collision avoidance
+            {
+                first += 10;
+            }
+            break;
+            
+        case 's':
+            if (second > -80) // collision avoidance
+            {
+                second -= 10;
+            }
+            break;
+            
+        case 'S':
+            if (second < 50) // collision avoidance
+            {
+                second += 10;
+            }
+            break;
+            
+        case 'o':
+            if (bucket < 20) // collision avoidance
+            {
+                bucket += 10;
+            }
+            break;
+            
+        case 'c':
+            if (bucket > -120) // collision avoidance
+            {
+                bucket -= 10;
+            }
+            break;
+        
+        case 'L' :
+            // switch between observer and scene coordinate system for the light source
+            
+            break;
+        case '1':
+            // change velocity of animation, duration 1 sec
+            break;
+        case '2':
+            // change velocity of animation, duration 2 sec
+            break;
+            
+        case '3':
+            // change velocity of animation, duration 3 sec
+            break;
+        
+        case '4':
+            // change velocity of animation, duration 4 sec
+            break;
+        
+        case '5':
+            // change velocity of animation, duration 5 sec
+            break;
+        
+            
+    }
+    
+    glutPostRedisplay();
 	}
-}
+
 
 int main(int argc, char** argv) 
 {
 	glutInit(&argc, argv);
 
 	//init display mode (double buffer, RGB, z-buffer support)
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	//glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    
+    /*
 	//initialize window
 	glutInitWindowSize(windowWidth, windowHeight); 
 	glutCreateWindow("LightExample");
 	init();
+     */
+    
+    //initialize window size and position. open window
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(200, 200);
+    glutCreateWindow("OpenGL - CGR");
+    
+    //call initialization routine
+    init();
 
 	//register callback functions
 	glutDisplayFunc(display);
